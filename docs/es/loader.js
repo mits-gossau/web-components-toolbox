eb3456fa261828ef3fc97fa7e21f03a474ddb970
Weedshaker <weedshaker@gmail.com>
2022-02-10 17:20:03 +0100

    template path fix loader path fix

--- docs/es/loader.js
+++ docs/es/loader.js
@@ -1,10 +1,10 @@
 const script = document.createElement('script')
-script.setAttribute('src', '../../../../../../wc-config.js?triggerImmediately=true')
+script.setAttribute('src', location.href.replace(/\/(?:.(?!src\/))+$/g, '/wc-config.js?triggerImmediately=true'))
 document.head.appendChild(script)
 const div = document.createElement('div')
 div.innerHTML = `
-    <link href=../../../../../css/colors.css rel=stylesheet type="text/css">
-    <link href=../../../../../css/fonts.css rel=stylesheet type="text/css">
-    <link href=../../../../../css/variables.css rel=stylesheet type="text/css">
+    <link href=${location.href.replace(/\/(?:.(?!src\/))+$/g, '/src/css/colors.css')} rel=stylesheet type="text/css">
+    <link href=${location.href.replace(/\/(?:.(?!src\/))+$/g, '/src/css/fonts.css')} rel=stylesheet type="text/css">
+    <link href=${location.href.replace(/\/(?:.(?!src\/))+$/g, '/src/css/variables.css')} rel=stylesheet type="text/css">
   `
 Array.from(div.children).forEach(child => document.head.appendChild(child))
