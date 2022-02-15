65dc582dd17dabd349fcea2491efa6c6221eebf4
Weedshaker <weedshaker@gmail.com>
2022-02-15 11:06:00 +0100

    morphed from analytics to gtm

--- src/es/components/controllers/tagManager/TagManager.js
+++ src/es/components/controllers/tagManager/TagManager.js
@@ -12,6 +12,11 @@ import { Shadow } from '../../prototypes/Shadow.js'
  *  {has} [wc-config-load=n.a.] trigger the render
  *  {number} [timeout=n.a.] timeout to trigger the render
  * }
+ * @example {
+    <c-tag-manager id="GTM-XXXXXX" wc-config-load>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXX" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    </c-tag-manager>
+ * }
  */
 export default class TagManager extends Shadow() {
   constructor (...args) {
@@ -51,32 +56,26 @@ export default class TagManager extends Shadow() {
    * @return {void}
    */
   render () {
-    const script = document.createElement('script')
-    script.textContent = /* html */`
-      window.ga = window.ga || function () { (ga.q = ga.q || []).push(arguments) }; ga.l = +new Date;
-      ga('create', '${this.getAttribute('id')}', 'auto');
-      ga('send', 'pageview');
-    `
-    this.html = script
-    this.loadDependency()
+    let script
+    if ((script = this.setup())) this.html = script
   }
 
   /**
-   * fetch dependency
+   * setup tag manager
    *
-   * @returns {Promise<{components: any}>}
+   * @param {string} [gtmId=this.getAttribute('id') || 'GTM-XXXXXX']
+   * @return {HTMLScriptElement|false}
    */
-  loadDependency () {
-    return this.dependencyPromise || (this.dependencyPromise = new Promise(resolve => {
-      const gtmScript = document.createElement('script')
-      gtmScript.setAttribute('type', 'text/javascript')
-      // TODO: Should Integrity check? https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity
-      gtmScript.setAttribute('src', 'https://www.google-analytics.com/analytics.js')
-      gtmScript.onload = () => {
-        resolve()
-      }
-      this.html = gtmScript
-    }))
+  setup (gtmId = this.getAttribute('id') || 'GTM-XXXXXX') {
+    // @ts-ignore
+    self.dataLayer = self.dataLayer || []
+    // cookie domain error when only localhost in url bar (only for local debugging)
+    if (self.location.hostname !== 'localhost') {
+      const script = document.createElement('script')
+      script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');`
+      return script
+    }
+    return false
   }
 
   get scripts () {
