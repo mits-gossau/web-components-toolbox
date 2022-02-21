fb2122fd3acfc71ccf5f60bbb2ded600f654982b
edmgb <evangelos.dinopoulos@mgb.ch>
2022-02-21 08:56:29 +0100

    feat: add quaternary button

---
+++ src/es/components/atoms/button/quaternary-/quaternary-.js
@@ -0,0 +1,3 @@
+// For Demo Purposes Only
+document.body.setAttribute('style', 'background-color: #eee;')
+if (document.querySelector('o-body')) document.querySelector('o-body').setAttribute('style', 'background-color: #eee;')
