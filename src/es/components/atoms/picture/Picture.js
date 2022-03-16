3a6a8bda302ac09de564d0df049f16b41c7930fc
Weedshaker <weedshaker@gmail.com>
2022-03-16 19:03:14 +0100

    picture first loading lazy

--- src/es/components/atoms/picture/Picture.js
+++ src/es/components/atoms/picture/Picture.js
@@ -263,10 +263,8 @@ export default class Picture extends Shadow() {
           composed: true
         }))
       })
-      this.img.setAttribute('loading', 'eager') // must load eager, not that the loading event doesn't trigger emit picture-load
-    } else {
-      this.img.setAttribute('loading', this.getAttribute('loading') || 'lazy')
     }
+    this.img.setAttribute('loading', this.getAttribute('loading') || 'lazy')
   }
 
   get img () {
