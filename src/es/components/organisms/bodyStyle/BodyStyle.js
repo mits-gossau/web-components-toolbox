69548d50a470dacf53eb024068f35ec4db645a4e
Weedshaker <weedshaker@gmail.com>
2022-03-17 08:56:32 +0100

    body style take mobile attributes

--- src/es/components/organisms/bodyStyle/BodyStyle.js
+++ src/es/components/organisms/bodyStyle/BodyStyle.js
@@ -37,12 +37,25 @@ export default class BodyStyle extends Body {
     const bodyCss = this.css.replace(/\s>\smain/g, '')
     this.css = ''
     this._css.textContent = bodyCss
+    const attributesMobile = []
     this.css = /* css */`
       :host {
         ${Array.from(this.attributes).reduce((acc, attribute) => {
-          if (attribute?.value === '') return acc
+          if (!attribute || !attribute.name || !attribute.value) return acc
+          if (attribute.name.includes('-mobile')) {
+            attributesMobile.push(attribute)
+            return acc
+          }
           return `${acc}${attribute.name}: ${attribute.value};--${attribute.name}: ${attribute.value};`
         }, '')}
+      }
+      @media only screen and (max-width: _max-width_) {
+        :host {
+          ${Array.from(attributesMobile).reduce((acc, attribute) => {
+            const attributeName = attribute.name.replace('-mobile', '')
+            return `${acc}${attributeName}: ${attribute.value};--${attributeName}: ${attribute.value};`
+          }, '')}
+        }
       }
     `
   }
