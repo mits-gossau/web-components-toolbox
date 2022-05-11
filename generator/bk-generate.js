007d02539f2ee35bdf450210eef0c6bd4c9fd9b4
edmgb <evangelos.dinopoulos@mgb.ch>
2022-05-11 12:05:41 +0200

    add new git repo

--- generator/bk-generate.js
+++ generator/bk-generate.js
@@ -1,9 +1,9 @@
 const shell = require('shelljs')
 
 // CHANGE THIS
-const GIT_REPO = 'https://github.com/mits-gossau/web-components-toolbox-parkimgruene.git'
+const GIT_REPO = 'https://github.com/mits-gossau/web-components-toolbox-logistikplattform.git'
 const NEW_PROJECT_DIR = ''
-const NEW_PROJECT_NAME = 'web-components-toolbox-parkimgruene'
+const NEW_PROJECT_NAME = 'web-components-toolbox-logistikplattform'
 
 // LEAVE THIS AS IT IS!
 const REF_REPO = 'https://github.com/mits-gossau/web-components-toolbox-betriebsrestaurant.git'
