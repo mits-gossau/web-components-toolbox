9f85439214584e72c8517a5617896ee9d37f60a0
Weedshaker <weedshaker@gmail.com>
2022-05-24 12:20:37 +0200

    portrait template added

--- src/es/components/molecules/carousel/Carousel.js
+++ src/es/components/molecules/carousel/Carousel.js
@@ -135,7 +135,7 @@ export default class MacroCarousel extends Shadow() {
     if (this.shouldComponentRenderCSS()) this.renderCSS()
     if (this.shouldComponentRenderHTML()) this.renderHTML()
     self.addEventListener('resize', this.resizeListener)
-    if (this.aPicture && this.aPicture.hasAttribute('picture-load') && !this.aPicture.hasAttribute('loaded')) this.addEventListener('picture-load', this.resizeListener)
+    if (this.aPictures.some(aPicture => aPicture.hasAttribute('picture-load') && !this.aPicture.hasAttribute('loaded'))) this.aPictures.forEach(aPicture => aPicture.addEventListener('picture-load', this.resizeListener, { once: true }))
     if (this.hasAttribute('sync-id')) {
       if (this.getAttribute('interval')) {
         this.macroCarousel.addEventListener('macro-carousel-selected-changed', this.macroCarouselSelectedChangedListener)
@@ -157,11 +157,12 @@ export default class MacroCarousel extends Shadow() {
       document.body.addEventListener('pause', this.focusEventListener, true)
     }
     this.resizeListener() // resets the picture calculations
+    setTimeout(() => this.resizeListener(), 200)
+    setTimeout(() => this.resizeListener(), 500)
   }
 
   disconnectedCallback () {
     self.removeEventListener('resize', this.resizeListener)
-    if (this.aPicture && this.aPicture.hasAttribute('picture-load') && !this.aPicture.hasAttribute('loaded')) this.removeEventListener('picture-load', this.resizeListener)
     if (this.hasAttribute('sync-id')) {
       if (this.getAttribute('interval')) {
         this.macroCarousel.removeEventListener('macro-carousel-selected-changed', this.macroCarouselSelectedChangedListener)
@@ -208,6 +209,9 @@ export default class MacroCarousel extends Shadow() {
    */
   renderCSS () {
     this.css = /* css */`
+      :host {
+        background: var(--background, none);
+      }
       :host > macro-carousel {
          width: var(--width, 100%) !important;
       }
@@ -312,6 +316,7 @@ export default class MacroCarousel extends Shadow() {
       }
       :host > #pagination {
         position: var(--pagination-position);
+        top: var(--pagination-top);
         bottom: var(--pagination-bottom);
       }
       :host div ::slotted(macro-carousel-pagination-indicator) {
@@ -329,6 +334,12 @@ export default class MacroCarousel extends Shadow() {
         --macro-carousel-navigation-button-size: var(--navigation-button-size, 6em);
         --macro-carousel-navigation-icon-size: var(--navigation-icon-size, 5em);
         --macro-carousel-navigation-icon-mask: var(--navigation-icon-mask, url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23000'%3E %3Cpath d='M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z'/%3E %3C/svg%3E")) ;
+      }
+      @media only screen and (max-width: ${this.mobileBreakpoint}) {
+        :host > #pagination {
+          top: var(--pagination-top-mobile, var(--pagination-top));
+          bottom: var(--pagination-bottom-mobile, var(--pagination-bottom));
+        }
       }
     `.replace(/var\(--/g, `var(--${this.namespace}`)
 
@@ -356,6 +367,17 @@ export default class MacroCarousel extends Shadow() {
         }, ...styles], false).then(fetchCSSParams => {
           fetchCSSParams[0].styleNode.textContent = fetchCSSParams[0].styleNode.textContent.replace(/--carousel-default-/g, '--carousel-emotion-')
         })
+      case 'carousel-portrait-':
+          return this.fetchCSS([{
+            path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}./default-/default-.css`, // apply namespace since it is specific and no fallback
+            namespace: false
+          }, {
+            // @ts-ignore
+            path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}./portrait-/portrait-.css`,
+            namespace: false
+          }, ...styles], false).then(fetchCSSParams => {
+            fetchCSSParams[0].styleNode.textContent = fetchCSSParams[0].styleNode.textContent.replace(/--carousel-default-/g, '--carousel-portrait-')
+          })
       default:
         return this.fetchCSS(styles, false)
     }
@@ -447,7 +469,7 @@ export default class MacroCarousel extends Shadow() {
     return this.root.querySelectorAll('script')
   }
 
-  get aPicture () {
-    return this.macroCarousel.querySelector('a-picture')
+  get aPictures () {
+    return Array.from(this.macroCarousel.querySelectorAll('a-picture'))
   }
 }
