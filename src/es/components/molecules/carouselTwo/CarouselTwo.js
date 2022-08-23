62624266a47b7e7fe1cd2aa12c5598cc829c33f6
Weedshaker <weedshaker@gmail.com>
2022-08-23 14:11:15 +0200

    stuff

--- src/es/components/molecules/carouselTwo/CarouselTwo.js
+++ src/es/components/molecules/carouselTwo/CarouselTwo.js
@@ -18,7 +18,39 @@ export default class MacroCarousel extends Shadow() {
   constructor (...args) {
     super(...args)
 
-    this.clickListener = event => this.root.querySelector(event.composedPath().find(node => typeof node.getAttribute === 'function' && node.getAttribute('href')).getAttribute('href')).scrollIntoView()
+    // on click scroll to the image with the matching id
+    this.clickListener = event => {
+      let target
+      if ((target = event.composedPath().find(node => typeof node.getAttribute === 'function' && node.getAttribute('href')))) {
+        this.root.querySelector(target.getAttribute('href')).scrollIntoView()
+        this.scrollListener()
+      }
+    }
+    // on focus scroll to the right element
+    this.focusListener = event => {
+      let target
+      if ((target = event.target) && Array.from(this.section.children).includes(target)) {
+        target.scrollIntoView()
+        this.scrollListener()
+      }
+    }
+    // on scroll calculate which image is shown and set its and all of its referencing href nodes the class to active
+    let scrollTimeoutId = null
+    const scrollTolerance = 5
+    this.scrollListener = event => {
+      clearTimeout(scrollTimeoutId)
+      scrollTimeoutId = setTimeout(() => {
+        let hostLeft, activeChild
+        if ((hostLeft = Math.round(this.getBoundingClientRect().left)) && (activeChild = Array.from(this.section.children).find(node => {
+          const nodeLeft = Math.round(node.getBoundingClientRect().left)
+          return hostLeft + scrollTolerance > nodeLeft && hostLeft - scrollTolerance < nodeLeft
+        }))) {
+          Array.from(this.root.querySelectorAll('.active')).forEach(node => node.classList.remove('active'))
+          activeChild.classList.add('active')
+          Array.from(this.root.querySelectorAll(`[href="#${activeChild.getAttribute('id')}"]`)).forEach(node => node.classList.add('active'))
+        }
+      }, 50)
+    }
   }
 
   connectedCallback () {
@@ -30,10 +62,14 @@ export default class MacroCarousel extends Shadow() {
       Promise.all(showPromises).then(() => (this.hidden = false))
     }
     this.addEventListener('click', this.clickListener)
+    this.section.addEventListener('scroll', this.scrollListener)
+    Array.from(this.section.children).forEach(node => node.addEventListener('focus', this.focusListener))
   }
 
   disconnectedCallback () {
     this.removeEventListener('click', this.clickListener)
+    this.section.removeEventListener('scroll', this.scrollListener)
+    Array.from(this.section.children).forEach(node => node.removeEventListener('focus', this.focusListener))
   }
 
   /**
@@ -61,22 +97,26 @@ export default class MacroCarousel extends Shadow() {
    */
   renderCSS () {
     this.css = /* css */`
-      .gallery {
+      :host > section {
         overflow: hidden;
         scroll-snap-type: x mandatory;
         scroll-behavior: smooth;
         display: flex;
       
       }
-      .gallery__img {
+      :host > section > * {
         min-width: 100%;
+        scroll-snap-align: start;
       }
-      .lil-nav {
+      :host > section > *:not(.active) {
+        visibility: hidden;
+      }
+      :host > nav {
         overflow-x: scroll;
         overflow-y: hidden;
         display: flex;
       }
-      .lil-nav a {
+      :host > nav > a {
         display: flex;
         margin-right: 10px;
       }
@@ -89,6 +129,15 @@ export default class MacroCarousel extends Shadow() {
    * @return {Promise<void>}
    */
   renderHTML () {
+    // below do those later and remove this.section getter
+    //shouldComponentRenderHTML () {
+    //return !this.section
+    //
+    //this.section = this.root.querySelector('section') || document.createElement('section')
     return Promise.resolve()
+  }
+
+  get section () {
+    return this.root.querySelector('section')
   }
 }
