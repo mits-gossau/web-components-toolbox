4c9ec48975993ce47f9963948d78684b1448a253
edmgb <evangelos.dinopoulos@mgb.ch>
2022-11-03 13:06:09 +0100

    WIP

--- src/es/components/msrc/productList/ProductList.js
+++ src/es/components/msrc/productList/ProductList.js
@@ -1,8 +1,8 @@
 // @ts-check
-import { Shadow } from '../../prototypes/Shadow.js'
+import { Prototype } from '../Prototype.js'
 
 
-export default class ProductList extends Shadow() {
+export default class ProductList extends Prototype() {
 
 
   _config = {
@@ -10,7 +10,6 @@ export default class ProductList extends Shadow() {
     language: 'de',
     webAPIKey: 'ZDsjzNwaw9AxGQWhzqMCsnjwYzwpQ7dzigdKXeuuiXeR97ao4phWLRwe2WrZRoPe',
     colCount: ['2', '2', '2', '4', '4'],
-    //colCount: ['3'],
     articlesPerPage: 10,
     filterOptions: {
       additionalQueryParams: {
@@ -34,288 +33,99 @@ export default class ProductList extends Shadow() {
     theme: 'mgb'
   }
 
-
-  // constructor(...args) {
-  //   super(...args)
-  //   // window.addEventListener('DOMContentLoaded', function () {
-  //   //   debugger
-  //   // })
-
-  // }
-
- 
-
-  async connectedCallback() {
-
-
-    setInterval(() => {
-      console.log("interval")
-      let componentStyles
-      let i = 0
-      if ((componentStyles = Array.from(document.querySelectorAll('style[data-styled]'))).length) {
-        componentStyles.forEach(componentStyle => {
-          if (componentStyle.sheet && componentStyle.sheet.rules && componentStyle.sheet.rules.length) {
-            Array.from(componentStyle.sheet.rules).forEach(rule => {
-              i++
-              //console.log(i, rule.cssText)
-              console.log(i)
-            })
-          }
-        })
-      }
-    }, 10000)
-    //this.css = this.getStyles()
-    
-    this.render()
-    const styles = await this.loadStyles()
-    console.log("styles", styles)
-    this.html = styles
-    this.loadDependency().then(async msrc => {
-      
-      const setup = JSON.parse(this.msrcWrapper.dataset.setup)
-      setup.webAPIKey = 'ZDsjzNwaw9AxGQWhzqMCsnjwYzwpQ7dzigdKXeuuiXeR97ao4phWLRwe2WrZRoPe'
-      setup.environment = 'production'
-      setup.mode = 'default'
-      
-      await msrc.components.articles.productList(this.widget, this._config)
-      await new Promise(resolve => setTimeout(() => resolve(), 5000))
-      this.html = this.getStyles(document.createElement('style'))
-
-    })
-
-
-
-
-
-    // this.loadStyles().then(styles=>{
-    //   console.log(styles)
-    //   this.html = styles
-
-    //   console.log(styles[0])
-
-    //   this.loadDependency().then(d => {
-    //    console.log(d)
-    //    this.render().then(r => {
-    //     console.log("render",r)
-    //     this.html = r
-    //      const setup = JSON.parse(this.msrcWrapper.dataset.setup)
-    //      setup.webAPIKey = 'ZDsjzNwaw9AxGQWhzqMCsnjwYzwpQ7dzigdKXeuuiXeR97ao4phWLRwe2WrZRoPe'
-    //      setup.environment = 'production'
-    //      setup.mode = 'default'
-    //      window.msrc.components.articles.productList(this.widget, setup)
-    //     const styles = this.getStyles()
-    //     console.log(styles)
-    //    }) 
-    //   })
-    // })
-
-    // const styles5 = document.createElement('style')
-    // styles5.textContent = '@import "https://fonts.googleapis.com/css?family=PT+Sans:700"'
-    // styles5.setAttribute('_css-msrc', '')
-    // styles5.setAttribute('protected', 'true') // this will avoid deletion by html=''
-    // this.html = styles5
-
-    // this.loadDependency().then(d => {
-    //   console.log("loaded", d)
-    //   this.render().then(h => {
-    //     console.log("h", h)
-    //     //this.html = h
-    //     return h
-    //   }).then(r => {
-
-    //     this.html = r
-
-
-    //     const setup = JSON.parse(this.msrcWrapper.dataset.setup)
-    //     setup.webAPIKey = 'ZDsjzNwaw9AxGQWhzqMCsnjwYzwpQ7dzigdKXeuuiXeR97ao4phWLRwe2WrZRoPe'
-    //     setup.environment = 'production'
-    //     setup.mode = 'default'
-    //     window.msrc.components.articles.productList(this.widget, setup)
-    //     console.log("rrrr",r)
-
-
-
-
-    //     const styleCheck = setInterval(() => {
-    //       //let st = this.getStyles()
-    //       //console.log(st)
-    //       //this.css = st
-
-    //       let componentStyles = Array.from(document.querySelectorAll('style[data-styled]'))
-    //       console.log("componentStyles", componentStyles)
-
-    //       const l2 = componentStyles[0].sheet.rules.length
-    //       console.log("l2", l2)
-
-
-    //       let cssText = ''
-    //       if (componentStyles.length) {
-    //         console.log("2222", componentStyles)
-    //         componentStyles.forEach(componentStyle => {
-    //           console.log("ööö", componentStyle.sheet.rules.length)
-    //           if (componentStyle.sheet && componentStyle.sheet.rules && componentStyle.sheet.rules.length) {
-    //             Array.from(componentStyle.sheet.rules).forEach(rule => {
-    //               //console.log(rule.cssText)
-    //               cssText += rule.cssText
-    //             })
-    //           }
-    //         })
-    //       }
-
-
-
-    //       this.css = cssText
-    //       // // st += cssText
-    //       // //this.css = st
-    //       // const sty = this.getStyles(document.createElement('style'))
-    //       // console.log(".....",sty)
-    //       // this.html = sty
-
-
-
-
-    //     }, 5000)
-    //   })
-
-
-
-
-    // })
+  connectedCallback() {
+    const showPromises = []
+    if (this.shouldComponentRender()) showPromises.push(this.render())
+    if (showPromises.length) {
+      this.hidden = true
+      Promise.all(showPromises).then(() => (this.hidden = false))
+    }
   }
 
-  async loadStyles() {
-
-    const styles3 = await document.createElement('style')
-    styles3.textContent = '@import "https://www.alnatura.ch/.resources/m5-bk-brand-theme/2.4.2-r84b41_64/css/styles.template.css"'
-    styles3.setAttribute('_css-msrc', '')
-    styles3.setAttribute('protected', 'true') // this will avoid deletion by html=''
-    //this.html = styles3
-
-    const styles6 = await document.createElement('style')
-    styles6.textContent = '@import "https://fonts.googleapis.com/css?family=PT+Sans:700"'
-    styles6.setAttribute('_css-msrc', '')
-    styles6.setAttribute('protected', 'true') // this will avoid deletion by html=''
-    //this.html = styles6
-
-    const styles4 = await document.createElement('style')
-    styles4.textContent = '@import "https://www.alnatura.ch/resources/templating-kit/themes/m5-bk-brand/sites/alnatura.css"'
-    styles4.setAttribute('_css-msrc', '')
-    styles4.setAttribute('protected', 'true') // this will avoid deletion by html=''
-    //this.html = styles4
-    return [styles3, styles6, styles4]
-
+  shouldComponentRender() {
+    return !this.msrcStoreFinderWrapper
   }
-
-  // shouldComponentRender() {
-  //   return !this.msrcProductListWrapper
-  // }
 
   render() {
-    // this.css = /* css */`
-    //   :host {
-    //     display:block;
-    //   }
-
-
-    //   }
-    // `
-    console.log("render start")
-    this.msrcWrapper = document.createElement('div')
-    this.msrcWrapper.className = "ui-js-product-list"
-    this.msrcWrapper.setAttribute('id', "msrc-options")
-    this.msrcWrapper.setAttribute('data-setup', '{"colCount":["2","2","2","4","4"],"fallbackEmptyComponent":"Es wurde kein Produkt gefunden. Bitte versuchen Sie es später noch einmal.","filterOptions":{"additionalQueryParams":{"limit":999,"view":"browseallretailers"},"category":["BeSS_97"],"fo":{"anchor_target":"_blank","link_target":"/de/produkte/{productSlug}.html","target":"alnatura"},"region":"gmzh"},"hideAddToShoppingList":true,"hideRating":false,"order":"asc","sort":"updated_at","theme":"mgb"}')
-
-    this.widget = document.createElement('div')
-    this.widget.setAttribute("id", "msrc-widget")
-
-    this.msrcWrapper.append(this.widget)
-
-    this.html = this.msrcWrapper
-
-
-  }
-
-
-
-  loadDependency() {
-    return this.dependencyPromise || (this.dependencyPromise = new Promise(resolve => {
-      const isMsrcLoaded = () => 'msrc' in self === true && 'utilities' in self.msrc === true && 'login' in self.msrc.utilities === true
-      if (isMsrcLoaded()) {
-        resolve(self.msrc) // eslint-disable-line
-      } else {
-
-        let scriptCount = 0
-
-        const vendorsMainScript = document.createElement('script')
-        vendorsMainScript.setAttribute('type', 'text/javascript')
-        vendorsMainScript.setAttribute('async', '')
-        vendorsMainScript.setAttribute('src', '//cdn.migros.ch/msrc/20211202125214/vendors~main.js')
-        vendorsMainScript.onload = () => {
-          scriptCount++
-          if (scriptCount >= 3) {
-            resolve(self.msrc) // eslint-disable-line
-          }
-        }
-
-        const mainScript = document.createElement('script')
-        mainScript.setAttribute('type', 'text/javascript')
-        mainScript.setAttribute('async', '')
-        mainScript.setAttribute('src', '//cdn.migros.ch/msrc/20211202125214/main.js')
-        mainScript.onload = () => {
-          scriptCount++
-          if (scriptCount >= 3) {
-            resolve(self.msrc) // eslint-disable-line
-          }
-        }
-
-        const productListScript = document.createElement('script')
-        productListScript.setAttribute('type', 'text/javascript')
-        productListScript.setAttribute('async', '')
-        productListScript.setAttribute('src', '//cdn.migros.ch/msrc/20211202125214/msrc-articles/productlist.js')
-        productListScript.onload = () => {
-          scriptCount++
-          if (scriptCount >= 3) {
-            resolve(self.msrc) // eslint-disable-line
-          }
-        }
-
-
-
-
-        this.html = [vendorsMainScript, mainScript, productListScript]
-
-
+    this.css = /* css */`
+      :host { 
       }
-    }))
-  }
-
-  getStyles(style) {
-    console.log("getStyle fn()", style)
-    let cssText = ''
-    const componentStyles = Array.from(document.querySelectorAll('style[data-styled]'))
-    if (componentStyles.length) {
-      console.log("cccc", componentStyles)
-      componentStyles.forEach(componentStyle => {
-        console.log("llllll", componentStyle.sheet.rules.length)
-        if (componentStyle.sheet && componentStyle.sheet.rules && componentStyle.sheet.rules.length) {
-          Array.from(componentStyle.sheet.rules).forEach(rule => {
-            //console.log(rule.cssText)
-            cssText += rule.cssText
-          })
-        }
-      })
-
-      if (style) {
-        style.textContent = cssText
-        style.setAttribute('_css-msrc', '')
-        style.setAttribute('protected', 'true') // this will avoid deletion by html=''
-        //console.log("in", cssText)
-        return style
+      @media only screen and (max-width: _max-width_) {   
       }
-      console.log("out", style)
-      return cssText
-    }
-    return false
+    `
+    return this.loadDependency().then(async msrc => {
+      this.msrcStoreFinderWrapper = this.root.querySelector('div') || document.createElement('div')
+      msrc.components.articles.productList(this.msrcStoreFinderWrapper, this._config)
+      const getStylesReturn = this.getStyles(document.createElement('style'))
+      this.html = [this.msrcStoreFinderWrapper, getStylesReturn[0]]
+    })
   }
 
+  // async loadStyles() {
+  //   const styles3 = await document.createElement('style')
+  //   styles3.textContent = '@import "https://www.alnatura.ch/.resources/m5-bk-brand-theme/2.4.2-r84b41_64/css/styles.template.css"'
+  //   styles3.setAttribute('_css-msrc', '')
+  //   styles3.setAttribute('protected', 'true') // this will avoid deletion by html=''
+  //   //this.html = styles3
+
+  //   const styles6 = await document.createElement('style')
+  //   styles6.textContent = '@import "https://fonts.googleapis.com/css?family=PT+Sans:700"'
+  //   styles6.setAttribute('_css-msrc', '')
+  //   styles6.setAttribute('protected', 'true') // this will avoid deletion by html=''
+  //   //this.html = styles6
+
+  //   const styles4 = await document.createElement('style')
+  //   styles4.textContent = '@import "https://www.alnatura.ch/resources/templating-kit/themes/m5-bk-brand/sites/alnatura.css"'
+  //   styles4.setAttribute('_css-msrc', '')
+  //   styles4.setAttribute('protected', 'true') // this will avoid deletion by html=''
+  //   //this.html = styles4
+  //   return [styles3, styles6, styles4]
+  // }
+
+
+  // loadDependency() {
+  //   return this.dependencyPromise || (this.dependencyPromise = new Promise(resolve => {
+  //     const isMsrcLoaded = () => 'msrc' in self === true && 'utilities' in self.msrc === true && 'login' in self.msrc.utilities === true
+  //     if (isMsrcLoaded()) {
+  //       resolve(self.msrc) // eslint-disable-line
+  //     } else {
+
+  //       let scriptCount = 0
+
+  //       const vendorsMainScript = document.createElement('script')
+  //       vendorsMainScript.setAttribute('type', 'text/javascript')
+  //       vendorsMainScript.setAttribute('async', '')
+  //       vendorsMainScript.setAttribute('src', '//cdn.migros.ch/msrc/20211202125214/vendors~main.js')
+  //       vendorsMainScript.onload = () => {
+  //         scriptCount++
+  //         if (scriptCount >= 3) {
+  //           resolve(self.msrc) // eslint-disable-line
+  //         }
+  //       }
+
+  //       const mainScript = document.createElement('script')
+  //       mainScript.setAttribute('type', 'text/javascript')
+  //       mainScript.setAttribute('async', '')
+  //       mainScript.setAttribute('src', '//cdn.migros.ch/msrc/20211202125214/main.js')
+  //       mainScript.onload = () => {
+  //         scriptCount++
+  //         if (scriptCount >= 3) {
+  //           resolve(self.msrc) // eslint-disable-line
+  //         }
+  //       }
+
+  //       const productListScript = document.createElement('script')
+  //       productListScript.setAttribute('type', 'text/javascript')
+  //       productListScript.setAttribute('async', '')
+  //       productListScript.setAttribute('src', '//cdn.migros.ch/msrc/20211202125214/msrc-articles/productlist.js')
+  //       productListScript.onload = () => {
+  //         scriptCount++
+  //         if (scriptCount >= 3) {
+  //           resolve(self.msrc) // eslint-disable-line
+  //         }
+  //       }
+  //       this.html = [vendorsMainScript, mainScript, productListScript]
+  //     }
+  //   }))
+  // }
 }
