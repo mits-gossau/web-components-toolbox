09ff573783ad55c0c02a92ba21c9b4b03f36cc11
edmgb <evangelos.dinopoulos@mgb.ch>
2022-12-20 16:02:39 +0100

    grid component setup

---
+++ src/es/components/organisms/grid/Grid.js
@@ -0,0 +1,89 @@
+// @ts-check
+/* global customElements */
+
+import { Shadow } from '../../prototypes/Shadow.js'
+
+/**
+ * Grid 
+ *
+ * @export
+ * @class Grid
+ * @type {CustomElementConstructor}
+ * @attribute {}
+ * @css {}
+ */
+export default class Grid extends Shadow() {
+  constructor (...args) {
+    super(...args)
+  }
+
+  connectedCallback () {
+    if (this.shouldComponentRenderCSS()) this.renderCSS()
+    if (this.shouldComponentRenderHTML()) this.renderHTML()
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldComponentRenderCSS () {
+    return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldComponentRenderHTML () {
+    return !this.grid
+  }
+
+  /**
+   * renders the o-grid css
+   *
+   * @return {Promise<void>}
+   */
+  renderCSS () {
+    this.css = /* css */`
+      :host {}
+      @media only screen and (max-width: _max-width_) {
+        :host {}
+      }
+    `
+    /** @type {import("../../prototypes/Shadow.js").fetchCSSParams[]} */
+    const styles = [
+      {
+        path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../../../css/reset.css`, // no variables for this reason no namespace
+        namespace: false
+      },
+      {
+        path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}../../../../css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
+        namespaceFallback: true
+      }
+    ]
+    switch (this.getAttribute('namespace')) {
+      case 'grid-2colums2rows-':
+        return this.fetchCSS([{
+          path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}./2colums2rows-/2colums2rows-.css`, // apply namespace since it is specific and no fallback
+          namespace: false
+        }, ...styles], false)
+      default:
+        return this.fetchCSS(styles)
+    }
+  }
+
+  /**
+   * renders the html
+   *
+   * @return {void}
+   */
+  renderHTML () {
+    this.grid = this.root.querySelector('div') || document.createElement('div')
+    this.grid.innerHTML = "Grid Content"
+    this.html = this.grid
+  }
+
+  
+}
