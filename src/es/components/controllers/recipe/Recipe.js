5dd11478a41d92f173566b7de5b84aae33ee44ff
edmgb <evangelos.dinopoulos@mgb.ch>
2022-12-30 08:05:02 +0100

    add recipe endpoint url attribute

--- src/es/components/controllers/recipe/Recipe.js
+++ src/es/components/controllers/recipe/Recipe.js
@@ -12,20 +12,34 @@ import { Shadow } from '../../prototypes/Shadow.js'
  * @type {CustomElementConstructor}
  */
 export default class Recipe extends Shadow() {
-  constructor (...args) {
+  constructor(...args) {
     super({ mode: 'false' }, ...args)
     this.abortController = null
 
     this.requestListRecipeListener = async event => {
       if (this.abortController) this.abortController.abort()
       this.abortController = new AbortController()
-      const fetchOptions = {
-        signal: this.abortController.signal
-      }
+
       const limit = (event.detail && event.detail.limit) || this.getAttribute('limit') || 0
       const currentSkip = event.detail && event.detail.skip ? Number(event.detail.skip) : 0
 
-      const endpoint = `https://testadmin.alnatura.ch/umbraco/api/AlnaturaRecipeApi/GetAllRecipes?limit=${limit}&offset=${currentSkip}`
+      const payload = {
+        limit: 9,
+        offset: 0,
+        filters: {}
+      }
+
+      const fetchOptions = {
+        method: 'POST',
+        headers: {
+          Accept: 'application.json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload),
+        signal: this.abortController.signal
+      }
+
+      const endpoint = this.getAttribute('endpoint')
       this.dispatchEvent(new CustomEvent(this.getAttribute('list-recipe') || 'list-recipe', {
         detail: {
           fetch: fetch(endpoint, fetchOptions).then(async response => {
@@ -35,7 +49,7 @@ export default class Recipe extends Shadow() {
               // TODO!!!
               return {
                 items: data.data.recipes.results,
-                limit: data.data.recipes.limit,
+                limit: 9,
                 skip: data.data.recipes.offset * 9,
                 tag: [],
                 total: 648
@@ -51,11 +65,11 @@ export default class Recipe extends Shadow() {
     }
   }
 
-  connectedCallback () {
+  connectedCallback() {
     this.addEventListener(this.getAttribute('request-list-recipe') || 'request-list-recipe', this.requestListRecipeListener)
   }
 
-  disconnectedCallback () {
+  disconnectedCallback() {
     this.removeEventListener(this.getAttribute('request-list-recipe') || 'request-list-recipe', this.requestListRecipeListener)
   }
 }
