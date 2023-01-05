823e228f05fcbc6df084dd75feeb2c7eaa815d8f
Weedshaker <weedshaker@gmail.com>
2023-01-06 00:06:56 +0100

    history navigation url save filters

--- src/es/components/msrc/productList/ProductList.js
+++ src/es/components/msrc/productList/ProductList.js
@@ -1,4 +1,14 @@
 // @ts-check
+/** @typedef {{
+  pushHistory?: boolean,
+  fetchSubTags: boolean,
+  clearSubTags: boolean,
+  isActive: boolean,
+  origEvent: CustomEvent,
+  tags: [string],
+  this: HTMLElement
+}} productListEventDetail */
+
 import { Prototype } from '../Prototype.js'
 
 /* global CustomEvent */
@@ -8,6 +18,12 @@ export default class ProductList extends Prototype() {
     super(...args)
     this.config = this.configSetup()
     this.requestListArticlesEventListener = event => this.widgetRenderSetup(event)
+    this.updatePopState = event => {
+      if (!event.state) return
+      /** @type {productListEventDetail} */
+      if (!event.detail) event.detail = { ...event.state }
+      this.widgetRenderSetup(event, false)
+    }
   }
 
   connectedCallback () {
@@ -21,10 +37,12 @@ export default class ProductList extends Prototype() {
       })
     }
     document.body.addEventListener(this.getAttribute('request-list-articles') || 'request-list-articles', this.requestListArticlesEventListener)
+    self.addEventListener('popstate', this.updatePopState)
   }
 
   disconnectedCallback () {
     document.body.removeEventListener(this.getAttribute('request-list-articles') || 'request-list-articles', this.requestListArticlesEventListener)
+    self.removeEventListener('popstate', this.updatePopState)
   }
 
   configSetup () {
@@ -40,29 +58,50 @@ export default class ProductList extends Prototype() {
     return setup
   }
 
-  widgetRenderSetup (event = null) {
-    if (event) this.config.filterOptions.category = event.detail.tags || [event.detail.category]
+  /**
+   *
+   *
+   * @param {{detail: productListEventDetail} | null} [event=null]
+   * @return {Promise<[void, any]>}
+   */
+  async widgetRenderSetup (event = null, setFilter = true) {
+    let detail
+    // check if it has a real event or else it renders the first-time after load at render
+    if (event && event.detail && event.detail.tags) {
+      if (setFilter) this.setFilter(event.detail)
+    } else if((detail = this.getFilter())) {
+      // check if this has no subTags and won't clear subTags plus if there has been a detailWithSubTags in the url pre-run it
+      if (!detail.fetchSubTags && !detail.clearSubTags) {
+        const detailWithSubTags = this.getFilter('detailWithSubTags')
+        if (detailWithSubTags) await this.widgetRenderSetup({detail: detailWithSubTags}, false)
+      }
+      event = {
+        detail
+      }
+    }
+    if (event) this.config.filterOptions.category = event.detail.tags
+    let subTagFetch
     this.dispatchEvent(new CustomEvent(this.getAttribute('list-articles') || 'list-articles', {
       detail: {
         this: this,
         config: this.config,
         msrcProductListWrapper: this.msrcProductListWrapper,
         tags: this.config.filterOptions.category,
-        subTagFetch: event && event.detail.fetchSubTags
+        subTagFetch: (subTagFetch = event && event.detail.fetchSubTags
           ? fetch((this.getAttribute('endpoint') ? this.getAttribute('endpoint') : 'https://testadmin.alnatura.ch/umbraco/api/ProductsApi/GetCats?cat=') + this.config.filterOptions.category).then(async response => {
             if (response.status >= 200 && response.status <= 299) {
               return await response.json()
             }
             throw new Error(response.statusText)
           })
-          : null,
+          : null),
         clearSubTags: event && event.detail.clearSubTags
       },
       bubbles: true,
       cancelable: true,
       composed: true
     }))
-    return this.msrc.components.articles.productList(this.msrcProductListWrapper, this.config)
+    return Promise.all([this.msrc.components.articles.productList(this.msrcProductListWrapper, this.config), subTagFetch])
   }
 
   shouldComponentRender () {
@@ -88,5 +127,39 @@ export default class ProductList extends Prototype() {
     :host h2 {
       font-family: "Helvetica Now Text XBold", var(--font-family-bold, var(--font-family, inherit));
     }`
+  }
+
+  /**
+   * Set detail and page in window.history
+   * @param {productListEventDetail} detail
+   * @return {void}
+   */
+  setFilter (detail) {
+    detail = {
+      pushHistory: detail.pushHistory,
+      fetchSubTags: detail.fetchSubTags,
+      clearSubTags: detail.clearSubTags,
+      tags: detail.tags
+    }
+    const url = new URL(location.href, location.href.charAt(0) === '/' ? location.origin : location.href.charAt(0) === '.' ? import.meta.url.replace(/(.*\/)(.*)$/, '$1') : undefined)
+    const detailValue = encodeURIComponent(JSON.stringify(detail))
+    url.searchParams.set('detail', detailValue)
+    // save the last with sub categories into the url
+    if (detail.fetchSubTags) url.searchParams.set('detailWithSubTags', detailValue)
+    if (detail.pushHistory !== false) history.pushState({ ...history.state, ...detail}, document.title, url.href)
+  }
+
+  /**
+   * Get detail from url else store
+   * @param {string} [name='detail]
+   * @return {productListEventDetail|null}
+   */
+  getFilter (name = 'detail') {
+    const urlParams = new URLSearchParams(location.search)
+    try {
+      return JSON.parse(decodeURIComponent(urlParams.get(name) || '')) || null
+    } catch (e) {
+      return null
+    }
   }
 }
