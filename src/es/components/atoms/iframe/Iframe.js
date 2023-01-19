545672ffdfb8496f214c6f80d72f856fbaa350af
Laurin-dev <laurin.graf@mgb.ch>
2023-01-19 10:54:09 +0100

    IFrame with max-height for portrait videos

--- src/es/components/atoms/iframe/Iframe.js
+++ src/es/components/atoms/iframe/Iframe.js
@@ -78,6 +78,7 @@ export default class Iframe extends Intersection() {
           ? `background-color: ${this.getAttribute('background-color')};`
           : ''
         }
+        max-height: 90vh;
       }
     `
   }
