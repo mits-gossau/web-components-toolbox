3dd597e3d8ea4a180c771a7fcb41e7723c845229
sturudic <srdan.turudic@emea.merkleinc.com>
2023-02-14 09:50:55 +0100

    feat: MUTOBOTEAM-1750 styling wrapper and picture

--- src/es/components/organisms/wrapper/Wrapper.js
+++ src/es/components/organisms/wrapper/Wrapper.js
@@ -218,6 +218,11 @@ export const Wrapper = (ChosenHTMLElement = Body) => class Wrapper extends Chose
           path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}./menu-legends-/menu-legends-.css`, // apply namespace since it is specific and no fallback
           namespace: false
         }, ...styles], false).then(() => this.calcColumnWidth())
+        case 'news-post-':
+        return this.fetchCSS([{
+          path: `${import.meta.url.replace(/(.*\/)(.*)$/, '$1')}./news-post-/news-post-.css`, // apply namespace since it is specific and no fallback
+          namespace: false
+        }, ...styles], false).then(() => this.calcColumnWidth())
       default:
         if (!this.hasAttribute('namespace')) {
           this.css = /* css */`
