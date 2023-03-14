eebf639236ebea455897d6a74824491c77c98b75
edmgb <evangelos.dinopoulos@mgb.ch>
2023-03-14 08:17:04 +0100

    linter

--- src/es/components/controllers/pinterestPixel/PinterestPixel.js
+++ src/es/components/controllers/pinterestPixel/PinterestPixel.js
@@ -18,8 +18,8 @@ import TagManager from '../tagManager/TagManager.js'
  * @example {
     <c-pinterest-pixel id="XXXXXXXXXXXXXXX" wc-config-load>
       <noscript>
-        <img height="1" width="1" style="display:none;" alt="" src=https://ct.pinterest.com/v3/?event=init&tid=2614324606034&pd[em]=<hashed_email_address>&noscript=1 />
-      </noscript>    
+        <img height="1" width="1" style="display:none;" alt="" src=https://ct.pinterest.com/v3/?event=init&tid=2614324606034&pd[em]=<hashed_email_address>&noscript=1 />
+      </noscript>
     </c-pinterest-pixel>
  * }
  */
@@ -41,10 +41,9 @@ export default class PinterestPixel extends TagManager {
         document.head.appendChild(link)
       }
       const script = document.createElement('script')
-      script.innerHTML = `!function(e){if(!window.pintrk){window.pintrk = function () {window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var n=window.pintrk;n.queue=[],n.version="3.0";var t=document.createElement("script");t.async=!0,t.src=e;var r=document.getElementsByTagName("script")[0]; r.parentNode.insertBefore(t,r)}}(https://s.pinimg.com/ct/core.js);pintrk('load', '${id}', {em: '<user_email_address>'});pintrk('page');`
+      script.innerHTML = `!function(e){if(!window.pintrk){window.pintrk = function () {window.pintrk.queue.push(Array.prototype.slice.call(arguments))};var n=window.pintrk;n.queue=[],n.version="3.0";var t=document.createElement("script");t.async=!0,t.src=e;var r=document.getElementsByTagName("script")[0]; r.parentNode.insertBefore(t,r)}}(https://s.pinimg.com/ct/core.js);pintrk('load', '${id}', {em: '<user_email_address>'});pintrk('page');`
       return script
     }
     return false
   }
 }
-
