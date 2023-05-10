d5760905c3605e14b1edb58dc8a880e80d146e55
Weedshaker <weedshaker@gmail.com>
2023-05-10 19:32:54 +0200

    fix: teaser in wrapper missing attributes fix

--- src/es/components/molecules/teaser/Teaser.js
+++ src/es/components/molecules/teaser/Teaser.js
@@ -266,6 +266,9 @@ export default class Teaser extends Intersection() {
     // accessible and seo conform a tag wrapped around this component
     if (this.hasAttribute('href') && this.parentNode) {
       const a = document.createElement('a')
+      Array.from(this.attributes).forEach(attribute => {
+        if (!attribute.name.includes('hidden')) a.setAttribute(attribute.name, attribute.value)
+      })
       a.setAttribute('wrapper', '')
       a.setAttribute('href', this.getAttribute('href'))
       a.setAttribute('target', this.getAttribute('target') || '_self')
--- src/es/components/organisms/wrapper/Wrapper.js
+++ src/es/components/organisms/wrapper/Wrapper.js
@@ -345,6 +345,9 @@ export const Wrapper = (ChosenHTMLElement = Body) => class Wrapper extends Chose
     // accessible and seo conform a tag wrapped around this component
     if (this.hasAttribute('href') && this.parentNode) {
       const a = document.createElement('a')
+      Array.from(this.attributes).forEach(attribute => {
+        if (!attribute.name.includes('hidden')) a.setAttribute(attribute.name, attribute.value)
+      })
       a.setAttribute('wrapper', '')
       a.setAttribute('href', this.getAttribute('href'))
       a.setAttribute('target', this.getAttribute('target') || '_self')
