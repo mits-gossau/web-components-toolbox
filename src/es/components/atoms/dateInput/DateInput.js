8dfd25f83cb8a6d504b8e66cca622b8930ff5076
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-07-05 15:04:30 +0200

    feat(index.html): add DateInput section with links to primary and secondary pages
    refactor(Basket.js): fix constructor indentation and remove unnecessary comments
    refactor(Basket.js): fix connectedCallback and disconnectedCallback indentation
    refactor(Basket.js): fix shouldRenderCSS and shouldRenderHTML indentation
    refactor(Basket.js): fix renderCSS and fetchTemplate indentation
    refactor(Basket.js): fix renderHTML indentation
    refactor(Basket.js): fix get icon indentation
    
    feat(dateInput.js): add DateInput component
    
    feat(dateInput): add primary date input component
    
    - Add primary date input CSS styles
    - Add primary date input HTML template
    - Add primary date input JavaScript file for demo purposes
    
    feat(dateInput): add secondary date input component
    
    - Add CSS file for the secondary date input component
    - Add HTML file for the secondary date input component
    - Include placeholder, calendar indicator, min and max attributes in the HTML file
    - Include disabled attribute in the second date input component
    - Load necessary web components for demo purposes
    
    Note: This commit does not include the JavaScript file for the secondary date input component.
    
    feat(dateInput): add secondary date input component for demo purposes
    
    This commit adds a new file `src/es/components/atoms/dateInput/secondary-/secondary-.js` which contains code for a secondary date input component. The component is added for demo purposes only and sets the background color of the body and root elements to `#eee`.

---
+++ src/es/components/atoms/dateInput/DateInput.js
@@ -0,0 +1,313 @@
+// @ts-check
+import { Shadow } from '../../prototypes/Shadow.js'
+
+/**
+ * DateInput
+ * An example at: default-/default-.html
+ *
+ * @export
+ * @class DateInput
+ * @type {CustomElementConstructor}
+ */
+
+export default class DateInput extends Shadow() {
+  static get observedAttributes () {
+    return ['label', 'disabled']
+  }
+
+  constructor (options = {}, ...args) {
+    super(
+      { hoverInit: undefined, importMetaUrl: import.meta.url, ...options },
+      ...args
+    )
+
+    this.clickListener = (event) => {
+      if (this.hasAttribute('disabled')) event.preventDefault()
+      if (this.getAttribute('request-event-name')) {
+        event.preventDefault()
+        this.dateInput.classList.toggle('active')
+        this.dateInput.setAttribute(
+          'aria-pressed',
+          this.dateInput.classList.contains('active')
+        )
+        this.dispatchEvent(
+          new CustomEvent(this.getAttribute('request-event-name'), {
+            detail: this.getEventDetail(event),
+            bubbles: true,
+            cancelable: true,
+            composed: true
+          })
+        )
+      }
+    }
+
+    this.answerEventListener = async (event) => {
+      let tags = event.detail.tags
+      if (this.getAttribute('active-detail-property-name')) {
+        tags = await this.getAttribute('active-detail-property-name')
+          .split(':')
+          .reduce(async (accumulator, propertyName) => {
+            // @ts-ignore
+            propertyName = propertyName.replace(/-([a-z]{1})/g, (match, p1) =>
+              p1.toUpperCase()
+            )
+            if (accumulator instanceof Promise) accumulator = await accumulator
+            return accumulator[propertyName]
+          }, event.detail)
+      }
+      if (tags) {
+        const tagsIncludesTag = this.hasAttribute('tag-search')
+          ? tags.some((tag) => tag.includes(this.getAttribute('tag-search')))
+          : tags.includes(this.getAttribute('tag'))
+        this.dateInput.classList[tagsIncludesTag ? 'add' : 'remove']('active')
+      }
+      this.dateInput.setAttribute(
+        'aria-pressed',
+        this.dateInput.classList.contains('active')
+      )
+    }
+  }
+
+  connectedCallback () {
+    super.connectedCallback()
+    if (this.shouldRenderCSS()) {
+      this.renderCSS()
+    }
+    if (this.shouldRenderHTML()) {
+      this.renderHTML()
+    }
+    this.dateInput.addEventListener('click', this.clickListener)
+    if (this.getAttribute('answer-event-name')) {
+      document.body.addEventListener(
+        this.getAttribute('answer-event-name'),
+        this.answerEventListener
+      )
+    }
+    this.attributeChangedCallback()
+    this.connectedCallbackOnce()
+  }
+
+  connectedCallbackOnce () {
+    if (document.body.hasAttribute('wc-config-load')) {
+      this.wcConfigLoadListener()
+    } else {
+      document.body.addEventListener(
+        this.getAttribute('wc-config-load') || 'wc-config-load',
+        this.wcConfigLoadListener,
+        { once: true }
+      )
+    }
+    this.connectedCallbackOnce = () => {}
+  }
+
+  disconnectedCallback () {
+    this.dateInput.removeEventListener('click', this.clickListener)
+    if (this.getAttribute('answer-event-name')) {
+      document.body.removeEventListener(
+        this.getAttribute('answer-event-name'),
+        this.answerEventListener
+      )
+    }
+  }
+
+  // @ts-ignore
+  attributeChangedCallback () {
+    this.hasAttribute('disabled')
+        ? this.dateInput.setAttribute('disabled', '')
+        : this.dateInput.removeAttribute('disabled')
+    this.hasAttribute('aria-disabled')
+        ? this.dateInput.setAttribute('aria-disabled', 'true')
+        : this.dateInput.removeAttribute('aria-disabled')
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderCSS () {
+    return !this.root.querySelector('style[_css]')
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderHTML () {
+    return !this.dateInput
+  }
+
+  renderCSS () {
+    this.css = /* css */ `
+        :host {
+            cursor: unset !important;
+            display: inline-block;
+        }
+        :host .date-input {
+            align-items: center;
+            background-color: var(--background-color, transparent);
+            border-radius: var(--border-radius, 0.5em);
+            border: var(--border-width, 0px) solid var(--border-color, transparent);
+            color: var(--color, black);
+            cursor: pointer;
+            display: flex;
+            font-family: var(--font-family, unset);
+            font-size: var(--font-size, 1em);
+            font-weight: var(--font-weight, 400);
+            justify-content: var(--justify-content, center);
+            letter-spacing: var(--letter-spacing, normal);
+            line-height: var(--line-height, 1.5em);
+            margin: var(--margin, auto);
+            opacity: var(--opacity, 1);
+            outline: var(--outline, none);
+            overflow: hidden;
+            padding: var(--padding, calc(0.75em - var(--border-width, 0px)) calc(1.5em - var(--border-width, 0px)));
+            text-decoration: var(--text-decoration, none);
+            text-transform: var(--text-transform, none);
+            touch-action: manipulation;
+            transition: var(--transition, background-color 0.3s ease-out, border-color 0.3s ease-out, color 0.3s ease-out);
+            width: var(--width, auto);
+        }
+        :host .date-input:hover, :host(.hover) .date-input {
+            background-color: var(--background-color-hover, var(--background-color, #B24800));
+            border: var(--border-width-hover, var(--border-width, 0px)) solid var(--border-color-hover, var(--border-color, #FFFFFF));
+            color: var(--color-hover, var(--color, #FFFFFF));
+            opacity: var(--opacity-hover, var(--opacity, 1));
+        }
+        :host .date-input:active, :host .date-input.active {
+            background-color: var(--background-color-active, var(--background-color-hover, var(--background-color, #803300)));
+            color: var(--color-active, var(--color-hover, var(--color, #FFFFFF)));
+        }
+        :host .date-input[disabled] {
+            border: var(--border-width-disabled, var(--border-width, 0px)) solid var(--border-color-disabled, var(--border-color, #FFFFFF));
+            background-color: var(--background-color-disabled, var(--background-color, #FFDAC2));
+            color: var(--color-disabled, var(--color, #FFFFFF));
+            cursor: not-allowed;
+            opacity: var(--opacity-disabled, var(--opacity, 1));
+            transition: opacity 0.3s ease-out;
+        }
+        :host .date-input[disabled]:hover, :host(.hover) .date-input[disabled]  {
+            opacity: var(--opacity-disabled-hover, var(--opacity-disabled, var(--opacity, 1)));
+        }
+        :host .date-input::placeholder {
+            color: var(--color, black);
+        }
+        :host .date-input[disabled]::placeholder {
+            color: var(--color-disabled, var(--color, #FFFFFF));
+            opacity: var(--opacity-disabled, var(--opacity, 0.5));
+        }
+        :host .date-input::-webkit-calendar-picker-indicator {
+            color: transparent;
+            background: none;
+            z-index: 1;
+        }
+        @media only screen and (max-width: _max-width_) {
+            :host .date-input {
+                font-size: var(--font-size-mobile, var(--font-size, 1em));
+                margin: var(--margin-mobile, var(--margin, auto));
+                border-radius: var(--border-radius-mobile, var(--border-radius, 0.571em));
+            }
+            :host .date-input:hover, :host(.hover) .date-input {
+                background-color: var(--background-color-hover-mobile, var(--background-color-hover, var(--background-color, #B24800)));
+                color: var(--color-hover-mobile, var(--color-hover, var(--color, #FFFFFF)));
+            }
+            :host .date-input:active, :host .date-input.active {
+                background-color: var(--background-color-active-mobile, var(--background-color-active, var(--background-color-hover, var(--background-color, #803300))));
+                color: var(--color-active-mobile, var(--color-active, var(--color-hover, var(--color, #FFFFFF))));
+            }
+        }
+    `
+    return this.fetchTemplate()
+  }
+
+  /**
+   * fetches the template
+   *
+   * @return {Promise<void>}
+   */
+  fetchTemplate () {
+    switch (this.getAttribute('namespace')) {
+      case 'date-input-primary-':
+        return this.fetchCSS([
+          {
+            // @ts-ignore
+            path: `${this.importMetaUrl}./primary-/primary-.css`,
+            namespace: false
+          }
+        ])
+      case 'date-input-secondary-':
+        return this.fetchCSS([
+          {
+            // @ts-ignore
+            path: `${this.importMetaUrl}./secondary-/secondary-.css`,
+            namespace: false
+          }
+        ])
+      default:
+        return Promise.resolve()
+    }
+  }
+
+  renderHTML () {
+    const dateInput = document.createElement('input')
+    const calendarIndicator = this.hasAttribute('calendarIndicator')
+      ? this.getAttribute('calendarIndicator')
+      : ''
+
+    dateInput.id = 'date-picker'
+    dateInput.className = 'date-input'
+    dateInput.name = dateInput.id
+    dateInput.type = 'text'
+
+    dateInput.onfocus = () => {
+      if (dateInput.value) {
+        const dateValue = dateInput.value.replace(calendarIndicator, '')
+        const parts = dateValue.split('.')
+        const formattedDate = `${parts[2]}-${parts[1].padStart(
+          2,
+          '0'
+        )}-${parts[0].padStart(2, '0')}`
+        dateInput.value = formattedDate
+      }
+      dateInput.type = 'date'
+      dateInput.showPicker()
+    }
+
+    dateInput.onblur = () => {
+      dateInput.type = 'text'
+    }
+
+    dateInput.onchange = () => {
+      if (dateInput.value) {
+        const formattedDate = new Date(dateInput.value).toLocaleDateString('de-CH')
+        dateInput.blur()
+        dateInput.value = formattedDate + calendarIndicator
+      }
+      if (!dateInput.value) {
+        dateInput.blur()
+      }
+    }
+
+    if (this.hasAttribute('min')) {
+      dateInput.setAttribute('min', this.getAttribute('min'))
+    }
+
+    if (this.hasAttribute('max')) {
+      dateInput.setAttribute('max', this.getAttribute('max'))
+    }
+
+    if (this.hasAttribute('placeholder')) {
+      dateInput.setAttribute(
+        'placeholder',
+        this.getAttribute('placeholder') + calendarIndicator
+      )
+    }
+
+    if (this.hasAttribute('disabled')) {
+        dateInput.setAttribute('disabled', this.getAttribute('disabled'))
+    }
+
+    this.html = dateInput
+  }
+}
