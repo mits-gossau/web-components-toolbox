125ce95cf95eb601bee7107d869284fa7bfd7b8f
edmgb <evangelos.dinopoulos@mgb.ch>
2023-07-05 17:59:26 +0200

    refactor user profile remove temp vars from metaHeader default css

--- src/es/components/atoms/userProfile/UserProfile.js
+++ src/es/components/atoms/userProfile/UserProfile.js
@@ -8,14 +8,19 @@ import { Shadow } from '../../prototypes/Shadow.js'
  */
 export default class UserProfile extends Shadow() {
 
-  constructor(...args) {
-    super(...args) 
+  constructor(options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, ...options }, ...args)
+    this.clickListener = event => {
+      console.log("hoi");
+      this.menuDiv?.classList.toggle("active");
+    }
   }
 
 
   connectedCallback() {
     if (this.shouldRenderCSS()) this.renderCSS()
     if (this.shouldRenderHTML()) this.renderHTML()
+    this.profileImgDiv?.addEventListener('click', this.clickListener)
   }
   disconnectedCallback() {
   }
@@ -35,7 +40,7 @@ export default class UserProfile extends Shadow() {
    * @return {boolean}
    */
   shouldRenderHTML() {
-    return !this.wrapper
+    return !this.profileImgDiv
   }
 
   /**
@@ -46,7 +51,17 @@ export default class UserProfile extends Shadow() {
   renderCSS() {
     this.css = /* css */`
       :host {
-        display:block;
+         display:block;
+      }
+      :host a {
+        display:flex !important;
+        align-items: center;
+        flex-direction: row;
+        margin:0 !important;
+        text-decoration:none !important;
+      }
+      :host span {
+        padding:0 0 0 0.5em;
       }
       :host > div {
         padding:0.8em;
@@ -54,20 +69,33 @@ export default class UserProfile extends Shadow() {
         border-radius:1.6em;
       }
       :host .profile {
-        height: 1.2em;
+        display:flex;
+      }
+      :host .profile:hover {
+        background-color:red;
+        cursor: pointer;
       }
       :host .profile img {
-       height: 1.2em; 
+        height: 1.2em; 
       }    
       :host .menu {
+        box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+        border-radius: 8px;
+        border: 1px solid  #F5F5F5;
         position: absolute;
         top: 3.9em;
         right: 5vw;
         padding: 0;
-        background: #fff;
+        background: #FFFFFF;
         width: auto;
-        border-radius: 15px;
-        z-index:9999999;
+        z-index:9999;
+        visibility: hidden;
+        opacity: 0;
+        transition: visibility 300ms linear, opacity 300ms linear;
+      }
+      :host .menu.active {
+        visibility: visible;
+        opacity: 1;
       }
       :host .menu ul {
         padding:var(--content-spacing) auto;
@@ -77,8 +105,6 @@ export default class UserProfile extends Shadow() {
         list-style: none;
         padding: calc(var(--content-spacing) / 2) var(--content-spacing);
         border-top: 1px solid rgba(0, 0, 0, 0.05);
-        display: flex;
-        align-items: center;
       }
       @media only screen and (max-width: _max-width_) {
         :host {}
@@ -93,14 +119,25 @@ export default class UserProfile extends Shadow() {
    * @return {Promise<void>}
    */
   fetchTemplate() {
+    /** @type {import("../../prototypes/Shadow.js").fetchCSSParams[]} */
+    const styles = [
+      {
+        path: `${this.importMetaUrl}../../../../css/reset.css`, // no variables for this reason no namespace
+        namespace: false
+      },
+      {
+        path: `${this.importMetaUrl}../../../../css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
+        namespaceFallback: true
+      }
+    ]
     switch (this.getAttribute('namespace')) {
       case 'user-profile-default-':
         return this.fetchCSS([{
           path: `${this.importMetaUrl}./default-/default-.css`, // apply namespace since it is specific and no fallback
           namespace: false
-        }])
+        }, ...styles], false)
       default:
-        return Promise.resolve()
+        return this.fetchCSS(styles, false)
     }
   }
 
@@ -110,29 +147,30 @@ export default class UserProfile extends Shadow() {
    * @return {void}
    */
   renderHTML() {
-    const menuDiv = document.createElement('div');
-    menuDiv.setAttribute('class', 'menu')
+    this.menuDiv = document.createElement('div');
+    this.menuDiv.setAttribute('class', 'menu')
     const menuUL = document.createElement('ul')
     const linkNodes = [...this.root.childNodes].filter(c => c.nodeName === 'A')
     this.html = ''
     linkNodes.forEach(node => {
-      console.log(node)
       const li = document.createElement('li');
       li.innerHTML = node.outerHTML
       menuUL.appendChild(li)
     })
-    menuDiv.appendChild(menuUL)
+    this.menuDiv.appendChild(menuUL)
     //
-    const profileImgDiv = document.createElement('div');
-    profileImgDiv.setAttribute('class', 'profile')
+    this.profileImgDiv = document.createElement('div');
+    this.profileImgDiv.setAttribute('class', 'profile')
     const profileImg = document.createElement('img')
     profileImg.setAttribute('src', this.icon)
-    profileImgDiv.appendChild(profileImg)
+    this.profileImgDiv.appendChild(profileImg)
     //
-    this.wrapper = this.root.querySelector('div') || document.createElement('div');
-    this.wrapper.appendChild(profileImgDiv)
-    this.wrapper.appendChild(menuDiv)
-    this.html = this.wrapper
+    //this.wrapper = this.root.querySelector('div') || document.createElement('div');
+    //this.wrapper.appendChild(profileImgDiv)
+    //this.wrapper.appendChild(menuDiv)
+    //this.html = this.wrapper
+    this.html = this.profileImgDiv
+    this.html = this.menuDiv
   }
 
   get icon() {
