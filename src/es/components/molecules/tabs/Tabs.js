1ab3546779db2f9f34a0f2b224a92ba075783eca
Alexander Schmidt <alexander.schmidt1@mgb.ch>
2023-09-07 13:07:00 +0200

    feat(index.html): add Tabs section to the HTML feat(Tabs.js): create Tabs component with renderCSS and renderHTML methods
    feat(tabs): add default tab component and its associated HTML and JS files

---
+++ src/es/components/molecules/tabs/Tabs.js
@@ -0,0 +1,57 @@
+// @ts-check
+import { Hover } from '../../prototypes/Hover.js'
+
+export default class Tabs extends Hover() {
+
+  connectedCallback () {
+    super.connectedCallback()
+    if (this.shouldRenderCSS()) this.renderCSS()
+    if (this.shouldRenderHTML()) this.renderHTML()
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderCSS () {
+    return !this.root.querySelector(
+      `:host > style[_css], ${this.tagName} > style[_css]`
+    )
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderHTML () {
+    return !this.svg
+  }
+
+  /**
+   * Renders the CSS.
+   *
+   * @return {void}
+   */
+  renderCSS () {
+    this.css = /* css */ `
+      :host {
+      }`
+  }
+
+  /**
+   * renders the html
+   *
+   * @return {void}
+   */
+  renderHTML () {
+    // TODO: SVG's should be taken from icons folder but fetch can't use cache and is too slow on loads of requests at once. object, img, etc. does not work for css styling. so most likely it needs a node script copying this stuff on update in the icon folder.
+    // TODO: or solve the problem with an icon controller with caching. Send event with Promise.resolve to controller, which then resolves it with the svg
+    // src/es/components/web-components-toolbox/src/icons/chevron_right.svg
+    this.html = /* html */ `
+      <div>Tabs</div>
+    `
+    this.html = this.style
+  }
+}
