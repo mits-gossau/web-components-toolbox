c899e87a03581b7cd1985907e338b50adf8310e7
Weedshaker <weedshaker@gmail.com>
2023-09-27 11:10:46 +0200

    flatpickr hidden stuff linter

--- src/es/components/atoms/flatpickr/Flatpickr.js
+++ src/es/components/atoms/flatpickr/Flatpickr.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { Shadow } from '../../prototypes/Shadow.js'
 
-/* global CustomEvent */
+/* global self */
 
 /**
  * Creates a Datepicker
@@ -14,12 +14,14 @@ export default class Flatpickr extends Shadow() {
   constructor (options = {}, ...args) {
     // @ts-ignore
     super({ hoverInit: undefined, importMetaUrl: import.meta.url, ...options, mode: 'false' }, ...args)
-
   }
 
   connectedCallback () {
-    if (this.shouldRenderCSS()) this.renderCSSPromise = this.renderCSS()
-    if (this.shouldRenderHTML()) this.renderHTMLPromise = this.renderHTML()
+    this.hidden = true
+    const showPromises = []
+    if (this.shouldRenderCSS()) showPromises.push(this.renderCSS())
+    if (this.shouldRenderHTML()) showPromises.push(this.renderHTML())
+    Promise.all(showPromises).then(() => (this.hidden = false))
   }
 
   disconnectedCallback () {
@@ -95,7 +97,7 @@ export default class Flatpickr extends Shadow() {
           namespace: false,
           styleNode: this.style,
           appendStyleNode: false
-        }])
+        }], false)
       default:
         return Promise.resolve()
     }
@@ -106,19 +108,19 @@ export default class Flatpickr extends Shadow() {
    *
    * @return {Promise<void>}
    */
-  renderHTML () {     
+  renderHTML () {
     return this.loadDependency().then(([flatpickr]) => {
-        const div = document.createElement('div')
-        div.textContent = 'pickr'
-        flatpickr(div, {
-            mode:'range',
-            onChange: (selectedDates, dateStr, instance) => {
-              div.textContent = dateStr
-            },
-        })
-        this.html = div
-        document.head.appendChild(this.style)
-        //https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.css 
+      const div = document.createElement('div')
+      div.textContent = 'pickr'
+      flatpickr(div, {
+        mode: 'range',
+        onChange: (selectedDates, dateStr, instance) => {
+          div.textContent = dateStr
+        }
+      })
+      this.html = div
+      document.head.appendChild(this.style)
+      // https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.css
     })
   }
 
@@ -130,29 +132,29 @@ export default class Flatpickr extends Shadow() {
   loadDependency () {
     // make it global to self so that other components can know when it has been loaded
     return this.flatpickr || (this.flatpickr = Promise.all([
-        new Promise((resolve, reject) => {
-            const script = document.createElement('script')
-            script.setAttribute('type', 'text/javascript')
-            script.setAttribute('async', '')
-            script.setAttribute('src', 'https://cdn.jsdelivr.net/npm/flatpickr')
-            script.setAttribute('integrity', 'sha384-5JqMv4L/Xa0hfvtF06qboNdhvuYXUku9ZrhZh3bSk8VXF0A/RuSLHpLsSV9Zqhl6')
-            script.setAttribute('crossorigin', 'anonymous')
-            // @ts-ignore
-            script.onload = () => self.flatpickr
-                // @ts-ignore
-                ? resolve(self.flatpickr)
-                : reject()
-            this.html = script
-        }),
-        new Promise(resolve => {
-            const style = document.createElement('link')
-            style.setAttribute('rel', 'stylesheet')
-            style.setAttribute('href', 'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.css')
-            style.setAttribute('crossorigin', 'anonymous')
-            // @ts-ignore
-            style.onload = () => resolve()
-            document.head.appendChild(style)
-        })
+      new Promise((resolve, reject) => {
+        const script = document.createElement('script')
+        script.setAttribute('type', 'text/javascript')
+        script.setAttribute('async', '')
+        script.setAttribute('src', 'https://cdn.jsdelivr.net/npm/flatpickr')
+        script.setAttribute('integrity', 'sha384-5JqMv4L/Xa0hfvtF06qboNdhvuYXUku9ZrhZh3bSk8VXF0A/RuSLHpLsSV9Zqhl6')
+        script.setAttribute('crossorigin', 'anonymous')
+        // @ts-ignore
+        script.onload = () => self.flatpickr
+        // @ts-ignore
+          ? resolve(self.flatpickr)
+          : reject(new Error('Flatpickr does not load into the global scope!'))
+        this.html = script
+      }),
+      new Promise(resolve => {
+        const style = document.createElement('link')
+        style.setAttribute('rel', 'stylesheet')
+        style.setAttribute('href', 'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.css')
+        style.setAttribute('crossorigin', 'anonymous')
+        // @ts-ignore
+        style.onload = () => resolve()
+        document.head.appendChild(style)
+      })
     ]))
   }
 
