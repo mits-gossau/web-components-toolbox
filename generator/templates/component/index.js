f213a06cee20eec79d6f29b04589d34d33099800
edmgb <evangelos.dinopoulos@mgb.ch>
2023-09-27 15:43:48 +0200

    JSDoc

--- generator/templates/component/index.js
+++ generator/templates/component/index.js
@@ -5,6 +5,11 @@ const ejs = require('ejs')
 const argv = require('yargs-parser')(process.argv.slice(2))
 const path = require('path')
 
+/**
+ * Takes in data and a template path, and renders default HTML, JavaScript, and CSS templates using the provided data.
+ * @param data - Object that contains the data to be passed to the EJS templates for rendering.
+ * @param templatePath - The path where the rendered templates will be saved. It is a string that represents the directory path where the output files will be created.
+ */
 const renderTemplates = (data, templatePath) => {
   const defaults = [
     { file: path.join(__dirname, './defaultHtml.ejs'), out: 'default-.html' },
@@ -21,6 +26,18 @@ const renderTemplates = (data, templatePath) => {
   })
 }
 
+/**
+ * Renders an EJS template file with provided data and options, and saves the
+ * rendered output to a specified directory with a specified filename.
+ * @param directoryPath - String that represents the path to the directory where the output file will be saved.
+ * @param filename - Path to the EJS template file that you want to render.
+ * It should be a string representing the file path, including the file extension (e.g., "template.ejs").
+ * @param data - Object that contains the data that will be used to render the EJS template.
+ * @param [options] - The `options` parameter is an optional object that can contain additional
+ * configuration options for rendering the file. These options can include things like the template
+ * engine settings, such as the delimiter, caching options, or any other specific options supported by
+ * the template engine being used.
+ */
 const renderFile = (directoryPath, filename, data, options = {}) => {
   ejs.renderFile(filename, data, options, (err, str) => {
     if (err) console.error(err)
@@ -30,6 +47,12 @@ const renderFile = (directoryPath, filename, data, options = {}) => {
   })
 }
 
+/**
+ * Creates a directory at the specified `directoryPath` and a subdirectory at
+ * the specified `templatePath`, and if the directories already exist, it deletes them and recreates them.
+ * @param directoryPath - Path where you want to create a new directory. This is the main directory that you want to create.
+ * @param templatePath - The path where you want to create a directory for templates.
+ */
 const makeFolder = (directoryPath, templatePath) => {
   try {
     fs.mkdirSync(directoryPath)
@@ -48,12 +71,16 @@ const makeFolder = (directoryPath, templatePath) => {
   }
 }
 
+/**
+ * Generates a template for a component based on the provided name and type.
+ */
 const main = () => {
   try {
     console.log('Generating template...')
     const { _: rest, name, type } = argv
 
     const data = {
+      rest,
       name: name.charAt(0).toUpperCase() + name.slice(1),
       folderName: name.charAt(0).toLowerCase() + name.slice(1),
       nameSpace: name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(),
@@ -68,9 +95,9 @@ const main = () => {
 
     const directoryPath = `./src/es/components/${data.type}/${data.folderName}/`
     const templatePath = `${directoryPath}/default-/`
+    const filename = path.join(__dirname, './component.ejs')
 
     makeFolder(directoryPath, templatePath)
-    const filename = path.join(__dirname, './component.ejs')
     renderFile(directoryPath, filename, data)
     renderTemplates(data, templatePath)
     console.log('Done. Reload File Explorer.')
