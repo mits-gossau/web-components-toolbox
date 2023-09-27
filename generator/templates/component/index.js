135cd4e313f9aef44fa68b36ae37f8704b335098
edmgb <evangelos.dinopoulos@mgb.ch>
2023-09-27 09:01:14 +0200

    cleanup

--- generator/templates/component/index.js
+++ generator/templates/component/index.js
@@ -5,40 +5,54 @@ const ejs = require('ejs')
 const argv = require('yargs-parser')(process.argv.slice(2))
 const path = require('path')
 
-const defaultTemplates = (data, templatePath) => {
-  const html = path.join(__dirname, './defaultHtml.ejs') 
-  const js = path.join(__dirname, './defaultJs.ejs') 
-  const css = path.join(__dirname, './defaultCss.ejs') 
+const renderTemplates = (data, templatePath) => {
+  const defaults = [
+    { file: path.join(__dirname, './defaultHtml.ejs'), out: 'default-.html' },
+    { file: path.join(__dirname, './defaultJs.ejs'), out: 'default-.js' },
+    { file: path.join(__dirname, './defaultCss.ejs'), out: 'default-.css' }
+  ]
+  defaults.forEach((f) => {
+    ejs.renderFile(f.file, data, {}, (err, str) => {
+      if (err) console.error(err)
+      const outputFile = `${templatePath}/${f.out}`
+      fs.ensureFileSync(outputFile)
+      fs.outputFileSync(outputFile, str)
+    })
+  })
+}
 
-  ejs.renderFile(html, data, {}, (err, str) => {
+const renderFile = (directoryPath, filename, data, options = {}) => {
+  ejs.renderFile(filename, data, options, (err, str) => {
     if (err) console.error(err)
-    const outputFile = `${templatePath}/default-.html`
+    const outputFile = path.join(process.cwd(), `${directoryPath}/${data.name}.js`)
     fs.ensureFileSync(outputFile)
     fs.outputFileSync(outputFile, str)
   })
+}
 
-  //
-  ejs.renderFile(js, data, {}, (err, str) => {
-    if (err) console.error(err)
-    const outputFile = `${templatePath}/default-.js`
-    fs.ensureFileSync(outputFile)
-    fs.outputFileSync(outputFile, str)
-  })
-  //
-  ejs.renderFile(css, data, {}, (err, str) => {
-    if (err) console.error(err)
-    const outputFile = `${templatePath}/default-.css`
-    fs.ensureFileSync(outputFile)
-    fs.outputFileSync(outputFile, str)
-  })
+const makeFolder = (directoryPath, templatePath) => {
+  try {
+    fs.mkdirSync(directoryPath)
+    fs.mkdirSync(templatePath)
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      // main dir
+      fs.rmSync(directoryPath, { recursive: true, force: true })
+      fs.mkdirSync(directoryPath)
+      // template dir
+      fs.rmSync(templatePath, { recursive: true, force: true })
+      fs.mkdirSync(templatePath)
+    } else {
+      throw err
+    }
+  }
 }
 
 const main = () => {
   try {
     console.log('Generating template...')
     const { _: rest, name, type } = argv
-    console.log(name,type)
-    console.log(argv)
+
     const data = {
       name: name.charAt(0).toUpperCase() + name.slice(1),
       folderName: name.charAt(0).toLowerCase() + name.slice(1),
@@ -47,43 +61,19 @@ const main = () => {
       typeShort: type.charAt(0)
     }
 
-    const options = {}
-
     if (!name || !type) {
       console.error('--name and --type flag required')
       process.exit(1)
     }
 
+    const directoryPath = `./src/es/components/${data.type}/${data.folderName}/`
+    const templatePath = `${directoryPath}/default-/`
+
+    makeFolder(directoryPath, templatePath)
     const filename = path.join(__dirname, './component.ejs')
-
-    ejs.renderFile(filename, data, options, (err, str) => {
-      if (err) console.error(err)
-      const directoryPath = `./src/es/components/${data.type}/${data.folderName}/`
-      const templatePath = `${directoryPath}/default-/` 
-      try {
-        fs.mkdirSync(directoryPath)
-        fs.mkdirSync(templatePath)
-      } catch (err) {
-        if (err.code === 'EEXIST') {
-          // dir
-          fs.rmSync(directoryPath, { recursive: true, force: true })
-          fs.mkdirSync(directoryPath)
-
-          //template dir
-          fs.rmSync(templatePath, { recursive: true, force: true })
-          fs.mkdirSync(templatePath)
-
-        } else {
-          throw err
-        }
-      }
-
-      const outputFile = path.join(process.cwd(), `${directoryPath}/${data.name}.js`)
-      fs.ensureFileSync(outputFile)
-      fs.outputFileSync(outputFile, str)
-      defaultTemplates(data, templatePath)
-      console.log('Done. Reload File Explorer.')
-    })
+    renderFile(directoryPath, filename, data)
+    renderTemplates(data, templatePath)
+    console.log('Done. Reload File Explorer.')
   } catch (err) {
     console.error(err)
   }
