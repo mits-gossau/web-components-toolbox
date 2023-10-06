c1853f36c1a201ec1891eb1080c892d6c60b7ae8
edmgb <evangelos.dinopoulos@mgb.ch>
2023-10-06 07:06:31 +0200

    cleanup generator

--- generator/templates/component/index.js
+++ generator/templates/component/index.js
@@ -76,38 +76,6 @@ const makeFolder = (directoryPath, templatePath) => {
  * Generates a template for a component based on the provided name and type.
  */
 const main = () => {
-  try {
-    console.log('Generating template...')
-    const { _: rest, name, type } = argv
-
-    const data = {
-      rest,
-      name: name.charAt(0).toUpperCase() + name.slice(1),
-      folderName: name.charAt(0).toLowerCase() + name.slice(1),
-      nameSpace: name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase(),
-      type,
-      typeShort: type.charAt(0)
-    }
-
-    if (!name || !type) {
-      console.error('--name and --type flag required')
-      process.exit(1)
-    }
-
-    const directoryPath = `./src/es/components/${data.type}/${data.folderName}/`
-    const templatePath = `${directoryPath}/default-/`
-    const filename = path.join(__dirname, './component.ejs')
-
-    makeFolder(directoryPath, templatePath)
-    renderFile(directoryPath, filename, data)
-    renderTemplates(data, templatePath)
-    console.log('Done. Reload File Explorer.')
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-const main2 = () => {
   inquirer.prompt(
     [
       {
@@ -120,8 +88,8 @@ const main2 = () => {
         name: 'name',
         message: 'What is the component name?',
         type: 'input',
-        validate: (firstName) => {
-          if (!firstName.length) {
+        validate: (name) => {
+          if (!name.length) {
             return 'Please provide a component name';
           }
           return true;
@@ -131,7 +99,9 @@ const main2 = () => {
     ]).then((answers) => {
       try {
         console.log('Generating template...')
+
         const { name, type } = answers
+
         const data = {
           name: name.charAt(0).toUpperCase() + name.slice(1),
           folderName: name.charAt(0).toLowerCase() + name.slice(1),
@@ -139,7 +109,7 @@ const main2 = () => {
           type: type +'s',
           typeShort: type.charAt(0)
         }
-        
+
         const directoryPath = `./src/es/components/${data.type}/${data.folderName}/`
         const templatePath = `${directoryPath}/default-/`
         const filename = path.join(__dirname, './component.ejs')
@@ -147,11 +117,13 @@ const main2 = () => {
         makeFolder(directoryPath, templatePath)
         renderFile(directoryPath, filename, data)
         renderTemplates(data, templatePath)
+
         console.log('Done. Reload File Explorer.')
+        
       } catch (err) {
         console.error(err)
       }
     });
 }
 
-main2()
+main()
