a1060cf968ea1fe94bc715e4828bef6ab8f65320
Roland Szabo <roland.szabo@mgb.ch>
2023-11-17 11:34:12 +0100

    Improved category filtering

--- src/es/components/controllers/product/Product.js
+++ src/es/components/controllers/product/Product.js
@@ -55,11 +55,20 @@ export default class Product extends Shadow() {
    * @param {{ detail: any; }} event
    */
   requestListProductListener = async (event) => {
+
+    let category = this.getCategory()
+
     if (event.detail && event.detail.tags) {
       this.setCategory(event.detail.tags[0], event.detail.pushHistory)
+      category = event.detail.tags[0]
     }
 
-    this.updateSortParam(event.detail)
+    // @ts-ignore
+    if (event.type === "popstate") this.updateSortParam(event.detail, false)
+
+    // @ts-ignore
+    if(event.type !== "popstate" && event.detail.type) this.updateSortParam(event.detail)
+  
 
     if (this.abortController) this.abortController.abort()
     this.abortController = new AbortController()
@@ -68,7 +77,6 @@ export default class Product extends Shadow() {
       signal: this.abortController.signal
     }
     const sortOrder = this.getSort() || ''
-    const category = this.getCategory()
     this.showSubCategories(this.subCategoryList, category)
     if (category !== null || event.detail.searchterm) {
       // @ts-ignore
@@ -138,6 +146,7 @@ export default class Product extends Shadow() {
   setCategory (category, pushHistory = true) {
     const url = new URL(location.href, location.href.charAt(0) === '/' ? location.origin : location.href.charAt(0) === '.' ? this.importMetaUrl : undefined)
     url.searchParams.set('category', category)
+    url.searchParams.delete('sort')
     if (pushHistory) history.pushState({ ...history.state, category }, document.title, url.href)
     return url
   }
