10cbe3185e5fd076f5a0574070a4242fab09b266
Weedshaker <weedshaker@gmail.com>
2024-03-11 16:21:07 +0100

    fix/(MenuIcon.js): Error on Line 55 parentElement undefined

--- src/es/components/atoms/menuIcon/MenuIcon.js
+++ src/es/components/atoms/menuIcon/MenuIcon.js
@@ -52,7 +52,7 @@ export default class MenuIcon extends Shadow() {
     if (this.shouldRenderHTML()) this.renderHTML()
     if (!this.hasAttribute('no-click') || this.getAttribute('click-event-name')) this.addEventListener('click', this.clickListener)
 
-    this.isCheckout = this.parentElement.getAttribute('is-checkout') === 'true'
+    this.isCheckout = this.parentElement?.getAttribute('is-checkout') === 'true'
      if (this.isCheckout) this.style.display = 'none'
   }
 
