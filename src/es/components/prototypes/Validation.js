962ef36b4a9f93b5024931bfa7a1d4a87ab7fe06
Roland Szabo <roland.szabo@mgb.ch>
2024-03-20 15:08:32 +0100

    Add as Json and create as proxy

--- src/es/components/prototypes/Validation.js
+++ src/es/components/prototypes/Validation.js
@@ -14,9 +14,17 @@ export const Validation = (ChosenClass = Shadow()) => class Validation extends C
     // TODO What if dont have submit button but only one validation field
     // TODO what if we had more forms? how to solve?
     this.submitButton = this.form.querySelector('input[type="submit"]')
-    this.allValidationNodes = Array.from(this.form.querySelectorAll('[m-v-rules]'))
+    this.allValidationNodes = Array.from(this.form.querySelectorAll('[data-m-v-rules]'))
     // TODO create new Validation class here
-    this.validationValues = Object.create({})
+    let handler = {
+      set: function whenChange(obj,prop,value) {
+        obj[prop] = value
+      console.log("The value of the object has changed!");
+      return true
+     }
+    }
+    this.validationValues = new Proxy({}, handler);
+
 
     if (this.allValidationNodes.length > 0) {
       this.allValidationNodes.forEach(node => {
@@ -28,8 +36,9 @@ export const Validation = (ChosenClass = Shadow()) => class Validation extends C
         // maybe we need to create this part of code as separated function
         if (node.hasAttribute('name')) {
           if (!this.validationValues.hasOwnProperty(node.getAttribute('name'))) {
+            console.log("rules", JSON.parse(node.getAttribute('data-m-v-rules')))
             this.validationValues[node.getAttribute('name')] = {
-              
+
             }
           }
         }
