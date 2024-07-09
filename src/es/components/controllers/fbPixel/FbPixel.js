8b7926b933928d5484c157d3ce7565ca91882a91
Alexander Schmidt <alexander@schmidt.eco>
2024-07-09 14:30:07 +0200

    refactor(fbPixel.js): update fbPixel script to include async and defer attributes for better performance

--- src/es/components/controllers/fbPixel/FbPixel.js
+++ src/es/components/controllers/fbPixel/FbPixel.js
@@ -39,7 +39,9 @@ export default class FbPixel extends TagManager {
         document.head.appendChild(link)
       }
       const script = document.createElement('script')
-      script.innerHTML = `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '${id}');fbq('track', 'PageView');`
+      script.setAttribute('async', '')
+      script.setAttribute('defer', '')
+      script.innerHTML = `setTimeout(function(){!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '${id}');fbq('track', 'PageView');}, 3000);`
       return script
     }
     return false
