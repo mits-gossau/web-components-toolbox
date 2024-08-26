71a523d27bf8c676c20d6a0442d9b7d96a6a27e3
Weedshaker <weedshaker@gmail.com>
2024-08-26 18:44:56 +0200

    download html

--- generator/download-generate.js
+++ generator/download-generate.js
@@ -1,26 +1,8 @@
-var http = require("http");
+var request = require("request");
 
-var options = {
-  host: 'www.klubschule.ch',
-  port: 80,
-  path: '/',
-  method: 'POST'
-};
-
-var req = http.request(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
-  });
-});
-
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
-});
-
-// write data to request body
-req.write('data\n');
-req.write('data\n');
-req.end();
+request(
+    { uri: "https://www.klubschule.ch" },
+    function(error, response, body) {
+        console.log(body);
+    }
+);
