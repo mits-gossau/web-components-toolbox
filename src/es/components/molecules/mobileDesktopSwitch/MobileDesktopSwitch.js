7433359e1501fc46dbe5507d27b1722aade1f37c
Weedshaker <weedshaker@gmail.com>
2025-02-14 16:08:57 +0100

    setup

---
+++ src/es/components/molecules/mobileDesktopSwitch/MobileDesktopSwitch.js
@@ -0,0 +1,93 @@
+// @ts-check
+import { Shadow } from '../../prototypes/Shadow.js'
+
+/**
+* @export
+* @class MobileDesktopSwitch
+* @type {CustomElementConstructor}
+*/
+export default class MobileDesktopSwitch extends Shadow() {
+  constructor (options = {}, ...args) {
+    super({ importMetaUrl: import.meta.url, ...options }, ...args)
+  }
+
+  connectedCallback () {
+    this.hidden = true
+    const showPromises = []
+    if (this.shouldRenderCSS()) showPromises.push(this.renderCSS())
+    if (this.shouldRenderHTML()) showPromises.push(this.renderHTML())
+    Promise.all(showPromises).then(() => (this.hidden = false))
+  }
+
+  disconnectedCallback () {}
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderCSS () {
+    return !this.root.querySelector(`${this.cssSelector} > style[_css]`)
+  }
+
+  /**
+   * evaluates if a render is necessary
+   *
+   * @return {boolean}
+   */
+  shouldRenderHTML () {
+    return !this.div
+  }
+
+  /**
+   * renders the css
+   * @returns Promise<void>
+   */
+  renderCSS () {
+    this.css = /* css */`
+      :host {}
+      @media only screen and (max-width: _max-width_) {
+        :host {}
+      }
+    `
+    return this.fetchTemplate()
+  }
+
+  /**
+   * fetches the template
+   */
+  fetchTemplate () {
+    /** @type {import("../../prototypes/Shadow.js").fetchCSSParams[]} */
+    const styles = [
+      {
+        path: `${this.importMetaUrl}../../../../css/reset.css`, // no variables for this reason no namespace
+        namespace: false
+      },
+      {
+        path: `${this.importMetaUrl}../../../../css/style.css`, // apply namespace and fallback to allow overwriting on deeper level
+        namespaceFallback: true
+      }
+    ]
+    switch (this.getAttribute('namespace')) {
+      case 'mobile-desktop-switch-default-':
+        return this.fetchCSS([{
+          path: `${this.importMetaUrl}./default-/default-.css`, // apply namespace since it is specific and no fallback
+          namespace: false
+        }, ...styles], false) // using showPromises @connectedCallback makes hide action inside Shadow.fetchCSS obsolete, so second argument hide = false
+      default:
+        return this.fetchCSS(styles)
+    }
+  }
+
+  /**
+   * Render HTML
+   * @returns Promise<void>
+   */
+  renderHTML () {
+    this.html = '<div>Content rendered from Component: MobileDesktopSwitch</div>'
+  }
+
+  get div () {
+    return this.root.querySelector('div')
+  }
+}
