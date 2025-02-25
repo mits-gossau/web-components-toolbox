d8e032dabf8345e204126b5a03ee7cc89187a220
edmgb <evangelos.dinopoulos@mgb.ch>
2025-02-25 14:48:51 +0100

    rename fn, add comments

--- generator/documenter/getMetaData.js
+++ generator/documenter/getMetaData.js
@@ -23,8 +23,8 @@ function getMetaData(filePath, options = { sourceType: 'module' }) {
       }
     })
     return {
-      summary: extractSummaryText(metaData, 'summary'),
-      integration: extractSummaryText(metaData, 'integration'),
+      summary: extractLeadingCommentByType(metaData, 'summary'),
+      integration: extractLeadingCommentByType(metaData, 'integration'),
     }
   } catch (error) {
     console.error(`Error parsing file: ${filePath} - ${error.message}`)
@@ -32,7 +32,13 @@ function getMetaData(filePath, options = { sourceType: 'module' }) {
   }
 }
 
-function extractSummaryText(array, searchKey = 'summary') {
+/**
+ * Extract the text from the leading comments that starts with the searchKey
+ * @param {array} array - the array of leading comments
+ * @param {string} searchKey - the key to search for in the leading comments
+ * @returns {string} - the extracted text
+ */
+function extractLeadingCommentByType(array, searchKey = 'summary') {
   const searchTag = `@${searchKey}`
 
   for (const element of array) {
@@ -43,6 +49,8 @@ function extractSummaryText(array, searchKey = 'summary') {
       let endIndex = textAfterTag.length
 
       // find the index of the next '@' symbol that starts a tag
+      // this is done to prevent text from a subsequent tag 
+      // from being included in the extracted text
       for (let i = 0; i < textAfterTag.length; i++) {
         // check for '@' preceded by whitespace (or at the very beginning, but i>0 prevents issues in this case)
         if (textAfterTag[i] === '@' && i > 0 && /\s/.test(textAfterTag[i - 1])) {
@@ -59,7 +67,7 @@ function extractSummaryText(array, searchKey = 'summary') {
       return textAfterTag.replace(/\*/g, '')
     }
   }
-  return 'n/a' 
+  return 'n/a'
 }
 
 module.exports = getMetaData
