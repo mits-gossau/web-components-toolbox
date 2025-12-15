cc17c209efce370891363e4c77333376f57836d3
Jose, Joel-MGB <Joel.Jose@mgb.ch>
2025-12-15 14:12:01 +0100

    remove console.log

--- src/es/components/organisms/footer/Footer.js
+++ src/es/components/organisms/footer/Footer.js
@@ -317,7 +317,6 @@ export default class Footer extends Shadow() {
               // a-picture generates it's html, when defaultSource
               if (clone.tagName !== 'A-PICTURE' || !clone.hasAttribute('defaultSource')) clone.html = node.html
               node.classList.add(`placeholder-node-${clones.length - 1}`)
-              console.log({node, clone})
             })
             // move all children into a dedicated div
             // create a summary/details for each sectionChild
